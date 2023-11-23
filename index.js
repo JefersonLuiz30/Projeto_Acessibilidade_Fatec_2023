4ae50f4e48b84188709678cdd7bcb67e600e740b
Jheferson <jhefersonlhuiz@gmail.com>
2023-11-23 20:41:49 -0300

    Atualizado

---
+++ index.js
@@ -0,0 +1,15 @@
+// function trocartema() {
+
+//     let fundo = document.querySelector('body');
+//     let trocar = document.querySelector('p');
+//     let h1 = document.querySelector('h1');
+//     let h2 = document.querySelector('h2');
+    
+         
+//     fundo.style.backgroundColor = "black";
+//     h1.style.color="white"; 
+//     h2.style.color="white"; 
+    
+    
+
+// }
